276f83ad17ea4779b05e3e258b07b7c6d16c30d7
Naveenkumar67 <naveenvempati4@gmail.com>
2021-02-26 13:36:45 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({slides}) => {
+  const [index,setIndex]=useState(0);
+  //console.log({slides});
+  const showTitle=()=>{
+    return <h1 data-testid="title">{slides[index].title}</h1>
+  }
+  const showText=()=>{
+    return <p data-testid="text">{slides[index].text}</p>
+  }
   return (
-    <></>
+    <>
+    {showTitle()}
+    {showText()}
+    <button
+    disabled={index==slides.length-1}
+    onClick={()=>setIndex(i+1)}
+    data-testid="button-next">
+      NEXT
+    </button>
+    <button
+    disabled={index==0}
+    onClick={()=>setIndex(i-1)}
+    data-testid="button-prev">
+      PREV
+    </button>
+    <button
+    disabled={index==0}
+    onClick={()=>setIndex(0)}
+    data-testid="button-next">
+      RESTART
+    </button>
+
+    </>
   )
 }
 
